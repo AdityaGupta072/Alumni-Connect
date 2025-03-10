a20c6547577241314af72b6a17a405e258a846bd
AdityaGupta072 <ag13102003@gmail.com>
2025-03-10 17:09:04 +0530

    Updated project after fixing ownership issue

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
-
 const express=require("express");
 const mongoose=require("mongoose");
 const cors =require("cors");
-
 const authRoute=require("./routes/authRoutes");
 const app=express();
 app.use(express.json());
@@ -12,13 +10,10 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB Connection Error:", err));
-
 // Use Routes
 app.use("/api/auth", authRoute);
-
 app.get("/", (req, res) => {
     res.send(" Alumni Connect API is Running!");
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
