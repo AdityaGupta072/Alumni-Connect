1e76a718ca9dcfe80c7c9cd848bbe835eb062341
AdityaGupta072 <ag13102003@gmail.com>
2025-03-10 17:29:28 +0530

    _

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,7 +11,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB Connection Error:", err));
 // Use Routes
-console.log("Added this message");
 app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
